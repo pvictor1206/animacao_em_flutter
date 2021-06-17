0b96626756fc96ca4ca01f74ca35916d9f115ab7
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2021-06-16 21:01:14 -0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  PlayPauseAnimation(),
+    );
+  }
+}
+
+class PlayPauseAnimation extends StatefulWidget {
+  const PlayPauseAnimation({Key? key}) : super(key: key);
+
+  @override
+  _PlayPauseAnimationState createState() => _PlayPauseAnimationState();
+}
+
+class _PlayPauseAnimationState extends State<PlayPauseAnimation> {
+  // Controller for playback
+  late RiveAnimationController _controller;
+
+  // Toggles between play and pause animation states
+  void _togglePlay() =>
+      setState(() => _controller.isActive = !_controller.isActive);
+
+  /// Tracks if the animation is playing by whether controller is running
+  bool get isPlaying => _controller.isActive;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = SimpleAnimation('idle');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RiveAnimation.asset(
+          'assets/casa.riv',
+          controllers: [_controller],
+          animations: ['idle'],
+          // Update the play state when the widget's initialized
+          onInit: () => setState(() {}),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _togglePlay,
+        tooltip: isPlaying ? 'Pause' : 'Play',
+        child: Icon(
+          isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
